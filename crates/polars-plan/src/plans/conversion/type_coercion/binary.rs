c6edb645756c4a202a29f5f9abb1d2026e9a027a
Simon Lin <simonlin.rqmmw@slmail.me>
2026-02-23 08:21:50 +1100

    c

--- crates/polars-plan/src/plans/conversion/type_coercion/binary.rs
+++ crates/polars-plan/src/plans/conversion/type_coercion/binary.rs
@@ -400,7 +400,8 @@ pub(super) fn coerce_comparison_literal(
     fn supertype_introduces_nulls_on_lhs(dtype_lhs: &DataType, supertype: &DataType) -> bool {
         supertype != dtype_lhs
             && dtype_lhs.is_integer()
-            && get_numeric_upcast_supertype_lossless(dtype_lhs, supertype).is_none()
+            && get_numeric_upcast_supertype_lossless(dtype_lhs, supertype).as_ref()
+                != Some(dtype_lhs)
     }
 
     if lit_rhs.is_null()
